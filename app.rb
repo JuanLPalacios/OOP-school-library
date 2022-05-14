6bc0ed4c6325b2e2726efa1241368c22a2997dc5
JuanLP <juan.luis.palacios.p@gmail.com>
2022-05-14 16:46:05 -0500

    Add UI structure

---
+++ app.rb
@@ -0,0 +1,13 @@
+class App
+  def list_all_books; end
+
+  def list_all_people; end
+
+  def create_a_person(is_teacher, name, age, aditional); end
+
+  def create_a_book(title, author); end
+
+  def create_a_rental(book, person, date); end
+
+  def list_all_rentals_for_person_id(id); end
+end
--- main.rb
+++ main.rb
@@ -1,42 +1,65 @@
-require './person'
-require './student'
-require './classroom'
-require './book'
-require './rental'
+require './app'
 
-person1 = Person.new(23, 'Jose')
-person2 = Person.new(19, 'Mary')
-person3 = Person.new(33, 'Jesus')
-classroom1 = Classroom.new('A-1')
-classroom2 = Classroom.new('B-2')
-classroom3 = Classroom.new('C-3')
-student1 = Student.new(21, classroom1, 'Joey')
-student2 = Student.new(22, classroom2, 'Beltran')
-student3 = Student.new(31, classroom2, 'Bickenstain')
-puts(classroom1.label)
-classroom1.students.each { |student| puts("  - #{student.name}") }
-puts(classroom2.label)
-classroom2.students.each { |student| puts("  - #{student.name}") }
-puts(classroom3.label)
-classroom3.students.each { |student| puts("  - #{student.name}") }
-book1 = Book.new('Book1', '')
-book2 = Book.new('Book2', '')
-book3 = Book.new('Book3', '')
-person2.add_rental(book1, '2021/11/02')
-book1.add_rental(person1, '2022/05/30')
-student2.add_rental(book2, '2021/09/09')
-book2.add_rental(student3, '2022/01/12')
-Rental.new(student2, book2, '2022/01/12')
-Rental.new(person2, book3, '2022/01/21')
-puts("#{person1.name} rentals:")
-person1.rentals.each { |rental| puts("  - #{rental.book.title} #{rental.date}") }
-puts("#{person2.name} rentals:")
-person2.rentals.each { |rental| puts("  - #{rental.book.title} #{rental.date}") }
-puts("#{person3.name} rentals:")
-person3.rentals.each { |rental| puts("  - #{rental.book.title} #{rental.date}") }
-puts("#{student1.name} rentals:")
-student1.rentals.each { |rental| puts("  - #{rental.book.title} #{rental.date}") }
-puts("#{student2.name} rentals:")
-student2.rentals.each { |rental| puts("  - #{rental.book.title} #{rental.date}") }
-puts("#{student3.name} rentals:")
-student3.rentals.each { |rental| puts("  - #{rental.book.title} #{rental.date}") }
+APP = App.new
+
+def list_all_books
+  APP.list_all_books
+end
+
+def list_all_people
+  APP.list_all_people
+end
+
+def create_a_person
+  is_teacher = gets.to_i == 1
+  name = gets.chomp
+  age = gets.to_i
+  aditional = gets.chomp
+  APP.create_a_person(is_teacher, name, age, aditional)
+end
+
+def create_a_book
+  title = gets.chomp
+  author = gets.chomp
+  APP.create_a_book(title, author)
+end
+
+def create_a_rental
+  book = APP.list_all_books[gets.to_i]
+  person = APP.list_all_people[gets.to_i]
+  date = gets.chomp
+  APP.create_a_rental(book, person, date)
+end
+
+def list_all_rentals_for_person_id
+  id = gets.to_i
+  APP.list_all_rentals_for_person_id(id)
+end
+
+# rubocop:disable Metrics/CyclomaticComplexity
+def main
+  puts 'Welcome to School Library App!\n'
+  loop do
+    # print main menu
+    option = gets.to_i
+    case option
+    when 1
+      list_all_books
+    when 2
+      list_all_people
+    when 3
+      create_a_person
+    when 4
+      create_a_book
+    when 5
+      create_a_rental
+    when 6
+      list_all_rentals_for_person_id
+    when 7
+      break
+    end
+  end
+end
+# rubocop:enable Metrics/CyclomaticComplexity
+
+main
